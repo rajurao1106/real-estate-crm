2c26a4765d84a9b9090273342b65516f5247f853
rajurao <rajurao1107@gmail.com>
2026-07-22 22:33:47 +0530

    first commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://real-estate-crm-r697.vercel.app/api",
   headers: { "Content-Type": "application/json" },
 });
 
